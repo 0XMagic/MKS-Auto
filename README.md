49b088500da67233f10e755dd72f15ecfe5168bd
JacobKessler <Javadotdet@gmail.com>
2025-06-30 15:10:17 -0400

    2.0 update
    The script now fully automates vtf creation from image sequence instead of just making the .mks file

--- README.md
+++ README.md
@@ -1,18 +1,19 @@
 # How to use
 
-1. Move the python file AND your images into your target directory.
-2. Drag and drop your .tga files onto the python file. (All non-tga files are ignored.)
-3. Name the output mks file when prompted.
+1. Set up config.json to your steamapps directory
+2. Drag and drop your .tga files onto the python script. (All non-tga files are ignored.)
+3. Name the output texture file when prompted.
 4. Enjoy!
 
-
 ### Making multiple sequences
-* Simply name your image files like this: "(Category)0(Filename).tga"
-* Each "category" will be its own sequence.
-* Categories are sorted alphabetically.
-* If you don't like the splitter character being 0, edit line 5 of the script.
 
+* Simply name your image files like this: "(Category)-(Filename).tga"
+* Each "category" will be its own sequence in the final texture.
+* Categories are sorted alphabetically.
+* If you don't like the splitter character being -, edit line 10 of the script.
 
 #### Notes:
-* Files are sorted alphabetically, so make sure they are named correctly to prevent your sequence from being out of order.
+
+* Files are sorted alphabetically, so make sure they are named correctly to prevent your sequences from being out of
+  order.
 * You DO NOT have to credit me if you use this script to make workshop items.
--- framemaker.py
+++ framemaker.py
@@ -1,24 +1,31 @@
+import os
 import sys
+import json
+import pathlib
+import subprocess
+import shutil
 
 
 class File:
-	CATEGORY_SPLITTER = "0"
+	CATEGORY_SPLITTER = "-"
 
 	def __init__(self, filepath: str):
+		self.fullpath = filepath.replace("\\", "/")
 		self.file = filepath.replace("\\", "/").split("/")[-1]
 		self.category = self.file.split(self.CATEGORY_SPLITTER)[0] if self.CATEGORY_SPLITTER in self.file else ""
 
 
 files = sorted([File(x) for x in sys.argv[1:] if x.lower().endswith(".tga")], key = lambda k: k.file)
 if not files:
-	input("ERROR: NO TGA FILES PROVIDED\nPress [enter] to quit.")
+	print("[ERROR] NO TGA FILES PROVIDED")
+	os.system("pause")
 	exit()
 
 categories = dict()
 for f in files:
 	if f.category not in categories:
 		categories[f.category] = list()
-	categories[f.category].append(f.file)
+	categories[f.category].append(f.fullpath)
 seq = 0
 output = list()
 for v in categories.values():
@@ -27,7 +34,61 @@ for v in categories.values():
 	for path in v:
 		output.append(f"frame {path} 1")
 	output.append("")
-output_dir = input("ENTER NAME OF TARGET DIRECTORY:\t").rstrip(".mks")
-with open(f"{output_dir}.mks", "w") as fl:
+output_name = ""
+while not output_name:
+	output_name = input("ENTER NAME OF MATERIAL:\t").lower()
+	if not output_name or any(x in output_name for x in "<>:\"/\\|?*") or not output_name.isascii():
+		print("Invalid name!")
+		output_name = ""
+
+output_dir = f"{output_name}.mks"
+output_sht = f"{output_name}.sht"
+output_tga = f"{output_name}.tga"
+
+with open(output_dir, "w") as fl:
 	fl.write("\n".join(output))
-input(f"{output_dir}.mks created\nPress [enter] to quit.")
+
+with open("config.json", "r") as fl:
+	_json = json.load(fl)
+	steam_dir = pathlib.Path(_json["SteamDir"])
+
+if not steam_dir.is_dir():
+	print("[ERROR] Steam is not found! Check config.json.")
+	os.system("pause")
+	exit()
+
+mks_dir = steam_dir / "common/Team Fortress 2/bin/mksheet.exe"
+
+if not mks_dir.is_file():
+	print("[ERROR] Team Fortress 2 is not installed on this device!")
+	os.system("pause")
+	exit()
+
+subprocess.call(str(mks_dir) + " " + output_dir)
+
+vtex_dir = steam_dir / "common/Team Fortress 2/bin/vtex.exe"
+tf_dir = steam_dir / "common/Team Fortress 2/"
+
+copy_dir = tf_dir / "tf/materialsrc" / output_name
+
+if not copy_dir.is_dir():
+	copy_dir.mkdir(parents = True)
+
+check_dir = copy_dir / output_dir
+check_sht = copy_dir / output_sht
+check_tga = copy_dir / output_tga
+
+if check_dir.is_file():
+	os.remove(check_dir)
+if check_sht.is_file():
+	os.remove(check_sht)
+if check_tga.is_file():
+	os.remove(check_tga)
+
+shutil.move(output_dir, copy_dir)
+shutil.move(output_sht, copy_dir)
+shutil.move(output_tga, copy_dir)
+
+output_sht = f"{copy_dir}/{output_sht}"
+
+subprocess.Popen(str(vtex_dir) + f" -game \"{str(tf_dir / 'tf')}\" \"" + output_sht + "\"")
